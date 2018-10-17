a22fd9d00e2dbc7cb9d021f270c295d6c5ad2e8a
Rashid Muhammad <rashidzmuhammad@DESKTOP-36UBKVU>
2018-10-17 17:50:29 -0400

    Added database and cookie session management logic

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
+const mongoose = require('mongoose');
 const express = require('express');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/user');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authroutes')(app);
 
