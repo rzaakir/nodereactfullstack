d61205ad435dc1dde6a0cacb89d103d5b9b48bd4
Rashid Muhammad <rashidzmuhammad@DESKTOP-36UBKVU>
2018-10-25 12:49:27 -0400

    Added logic to print running environment to the console.

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,7 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
     // production
+    console.log(process.env.NODE_ENV);
     module.exports = require('./prod');
 } else {
     // dev
+    console.log(process.env.NODE_ENV);   
     module.exports = require('./dev');
 }
